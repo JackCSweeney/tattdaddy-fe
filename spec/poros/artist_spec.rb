3c7b2a84f8db8f63a08d358320c0fe237837c1d1
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-10 15:54:36 -0600

    Test authenticate method

--- spec/poros/artist_spec.rb
+++ spec/poros/artist_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Artist do
+  before do
+    data = {
+      id: "322458",
+      name: "Tattoo artists",
+      location: "1400 U Street NW",
+      email: "tatart@gmail.com",
+      identity: "LGBTQ+ Friendly",
+      password_digest: "12345"
+    }
+
+    @artist = Artist.new(data)
+  end
+
   describe "initialize" do
-    before do
-      data = {
-        id: "322458",
-        name: "Tattoo artists",
-        location: "1400 U Street NW",
-        email: "tatart@gmail.com",
-        identity: "LGBTQ+ Friendly",
-        password_digest: "unreadable hash"
-      }
-
-      @artist = Artist.new(data)
-    end
-
     it "exists" do
       expect(@artist).to be_an(Artist)
     end
@@ -25,7 +25,13 @@ RSpec.describe Artist do
       expect(@artist.location).to eq("1400 U Street NW")
       expect(@artist.email).to eq("tatart@gmail.com")
       expect(@artist.identity).to eq("LGBTQ+ Friendly")
-      expect(@artist.password_digest).to eq("unreadable hash")
+      expect(@artist.password_digest).to eq("12345")
+    end
+  end
+
+  describe "authenticate" do
+    it "returns true when input password is correct" do
+      expect(@artist.authenticate("12345")).to be(true)
     end
   end
 end
