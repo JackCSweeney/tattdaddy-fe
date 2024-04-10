68110869b7444b1474624ebda0252cefb484e793
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-10 07:13:53 -0600

    Feat: create ArtistService connection, get_url and find_artists mehtods

---
+++ app/services/artist_service.rb
@@ -0,0 +1,14 @@
+class ArtistService
+  def connection
+    Faraday.new(url: "http://localhost:3000/api/v0")
+  end
+
+  def get_url(uri)
+    response = connection.get(uri)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def find_artists
+    get_url("artists")
+  end
+end
