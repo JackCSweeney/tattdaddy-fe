558873a2ee676ca235eaf0043de7334ca040589e
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-10 15:19:16 -0500

    create and test user service

---
+++ spec/services/user_service_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe UserService do
+  describe ".user_date(user_id)" do
+    it "returns a user's data given the user's id" do
+      json_response = File.read("spec/fixtures/user/user.json")
+
+      stub_request(:get, "http://localhost:3000/api/v0/users/25")
+        .to_return(status: 200, body: json_response)
+
+      user = UserService.user_data(25)
+
+      expect(user).to be_a(Hash)
+      expect(user[:data]).to be_a(Hash)
+      expect(user[:data][:type]).to eq("user")
+    end
+  end
+
+  describe ".dashboard_tattoos(user_id)" do
+    it "returns tattoo search results based on a user's preferences" do
+      json_response = File.read("spec/fixtures/user/dashboard_tattoos.json")
+
+      stub_request(:get, "http://localhost:3000/api/v0/tattoos?user=25")
+        .to_return(status: 200, body: json_response)
+
+      user_dashboard_tattoos = UserService.dashboard_tattoos(25)
+
+      expect(user_dashboard_tattoos).to be_a(Hash)
+      expect(user_dashboard_tattoos[:data]).to be_an(Array)
+      expect(user_dashboard_tattoos[:data].size).to eq(15)
+      expect(user_dashboard_tattoos[:data]).to all(be_a(Hash))
+      expect(user_dashboard_tattoos[:data]).to all(include(type: "tattoo"))
+    end
+  end
+end
