08a864fbac8ead31d4ae6c87296c8ddb9f2a0bab
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-10 06:15:45 -0600

    Feat: create action on SessionsController and private helper methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController <ApplicationController
+  def create
+    if params[:commit] == "Sign In as User"
+      user = User.find_by(email: params[:email])
+      authenticate_user(user)
+    elsif params[:commit] == "Sign In as Artist"
+      artist = Artist.find_by(email: params[:email])
+      authenticate_artist(artist)
+    end
+  end
+
+  private
+  def authenticate_user(user)
+    if user && user.authenticate(params[:password])
+      redirect_to user_dashboard_path(user)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def authenticate_artist(artist)
+    if artist && artist.authenticate(params[:password])
+      redirect_to artist_dashboard_path(artist)
+    else
+      redirect_to root_path
+    end
+  end
+end
