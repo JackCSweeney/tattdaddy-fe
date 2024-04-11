3ddb0b1d268cfa0efe6c7997f82a3ff9fc23fe5a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-11 11:53:56 -0600

    Rename to format

---
+++ app/facades/artist_facade.rb
@@ -0,0 +1,23 @@
+class ArtistFacade
+
+  def artists
+    service = ArtistService.new
+    json = service.find_artists
+    json[:data].map do |json_data|
+      data = artist_data(json_data)
+
+      Artist.new(data)
+    end
+  end
+
+  def artist_data(artist_json)
+    data = {
+      id: artist_json[:id],
+      name: artist_json[:attributes][:name],
+      location: artist_json[:attributes][:location],
+      email: artist_json[:attributes][:email],
+      identity: artist_json[:attributes][:identity],
+      password_digest: artist_json[:attributes][:password_digest],
+    }
+  end
+end
