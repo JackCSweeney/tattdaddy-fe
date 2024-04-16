add50b9db6dc45bf7cce78385f5f58455ad1cdb9
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-16 15:44:26 -0600

    Refactor facade to handle 400 responses from API calls

--- app/facades/artist_facade.rb
+++ app/facades/artist_facade.rb
@@ -1,48 +1,55 @@
 class ArtistFacade
-
   def artists
-    service = ArtistService.new
-    json = service.find_artists
-    json[:data].map do |json_data|
-      data = artist_data(json_data)
-
-      Artist.new(data)
-    end
+    json = artist_service.find_artists
+    json[:data].map { |json_data| build_artist(json_data) }
   end
 
-
   def find_artist(id)
-    json = ArtistService.new.find_artist(id)
-    data = artist_data(json[:data])
-    Artist.new(data)
+    json = artist_service.find_artist(id)
+    build_artist(json[:data])
   end
 
   def find_artist_tattoos(id)
-    service = ArtistService.new
-    json = service.artist_tattoos(id)
-    json[:data].map do |json_data|
-      Tattoo.new(json_data)
+    json = artist_service.artist_tattoos(id)
+    json[:data].map { |json_data| Tattoo.new(json_data) }
+  end
+
+  def update_tattoo(tattoo_attributes)
+    begin
+      data = artist_service.update_tattoo(tattoo_attributes[:tattoo][:id], tattoo_attributes)
+      find_tattoo(data[:data][:id])
+    rescue StandardError => e
+      handle_error(e)
     end
   end
 
+  def artist_service
+    @artist_service ||= ArtistService.new
+  end
+
+  def build_artist(json_data)
+    data = artist_data(json_data)
+    Artist.new(data)
+  end
+
   def artist_data(artist_json)
-    data = {
+    {
       id: artist_json[:id],
       name: artist_json[:attributes][:name],
       location: artist_json[:attributes][:location],
       email: artist_json[:attributes][:email],
       identity: artist_json[:attributes][:identity],
-      password_digest: artist_json[:attributes][:password_digest],
+      password_digest: artist_json[:attributes][:password_digest]
     }
   end
 
   def find_tattoo(id)
-    data = ArtistService.new.find_tattoo(id)
+    data = artist_service.find_tattoo(id)
     Tattoo.new(data[:data])
   end
 
-  def update_tattoo(tattoo_attributes)
-    ArtistService.new.update_tattoo(tattoo_attributes[:tattoo][:id], tattoo_attributes)
-    tattoo = find_tattoo(tattoo_attributes[:tattoo][:id])
+  def handle_error(error)
+    puts "An error occurred: #{error.message}"
+    nil
   end
 end
