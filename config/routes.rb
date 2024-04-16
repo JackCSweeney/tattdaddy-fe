b7880273a87e9b545086d3b6d1f4ca743d8f93e4
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-15 23:25:22 -0600

    setup sep. appts controller for user

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   resources :users, except: :index do
     get "/dashboard", to: "users/dashboard#show"
     resources :tattoos, only: [:index, :create, :destroy], controller: "users/tattoos"
-    resources :appointments, only: [:index]
+    resources :appointments, only: [:index], controller: "users/appointments"
   end
 
   resources :artists, except: :index do
@@ -25,38 +25,38 @@ Rails.application.routes.draw do
   end
 end
 
-  #             Prefix Verb   URI Pattern                                           Controller#Action
-  #               root GET    /                                                        welcome#index
-  #            sign_in POST   /sign_in(.:format)                                      sessions#create
-  #           sign_out DELETE /sign_out(.:format)                                     sessions#destroy
-  #     user_dashboard GET    /users/:user_id/dashboard(.:format)              users/dashboard#show
-  #       user_tattoos GET    /users/:user_id/tattoos(.:format)                  users/tattoos#index
-  #                    POST   /users/:user_id/tattoos(.:format)                  users/tattoos#create
-  #        user_tattoo DELETE /users/:user_id/tattoos/:id(.:format)               users/tattoos#destroy
-  #       user_tattoos GET    /users/:user_id/tattoos(.:format)                        tattoos#index
-  #  user_appointments GET    /users/:user_id/appointments(.:format)              appointments#index
-  #              users POST   /users(.:format)                                           users#create
-  #           new_user GET    /users/new(.:format)                                       users#new
-  #          edit_user GET    /users/:id/edit(.:format)                                  users#edit
-  #              user GET     /users/:id(.:format)                                       users#show
-  #                    PATCH  /users/:id(.:format)                                       users#update
-  #                    PUT    /users/:id(.:format)                                       users#update
-  #                    DELETE /users/:id(.:format)                                       users#destroy
+  #             Prefix Verb   URI Pattern                                 Controller#Action
+  #               root GET    /                                              welcome#index
+  #            sign_in POST   /sign_in                                      sessions#create
+  #           sign_out DELETE /sign_out                                     sessions#destroy
+  #     user_dashboard GET    /users/:user_id/dashboard              users/dashboard#show
+  #       user_tattoos GET    /users/:user_id/tattoos                  users/tattoos#index
+  #                    POST   /users/:user_id/tattoos                  users/tattoos#create
+  #        user_tattoo DELETE /users/:user_id/tattoos/:id               users/tattoos#destroy
+  #       user_tattoos GET    /users/:user_id/tattoos                        tattoos#index
+  #  user_appointments GET    /users/:user_id/appointments        users/appointments#index
+  #              users POST   /users                                           users#create
+  #           new_user GET    /users/new                                       users#new
+  #          edit_user GET    /users/:id/edit                                  users#edit
+  #              user GET     /users/:id                                       users#show
+  #                    PATCH  /users/:id                                       users#update
+  #                    PUT    /users/:id                                       users#update
+  #                    DELETE /users/:id                                       users#destroy
 
 
-#               Prefix Verb   URI Pattern                                           Controller#Action
-#     artist_dashboard GET    /artists/:artist_id/dashboard(.:format)        artists/dashboard#show
-#       artist_tattoos POST   /artists/:artist_id/tattoos(.:format)                    tattoos#create
-#    new_artist_tattoo GET    /artists/:artist_id/tattoos/new(.:format)                tattoos#new
-#   edit_artist_tattoo GET    /artists/:artist_id/tattoos/:id/edit(.:format)           tattoos#edit
-#        artist_tattoo PATCH  /artists/:artist_id/tattoos/:id(.:format)                tattoos#update
-#                      PUT    /artists/:artist_id/tattoos/:id(.:format)                tattoos#update
-#                      DELETE /artists/:artist_id/tattoos/:id(.:format)                tattoos#destroy
-#  artist_appointments GET    /artists/:artist_id/appointments(.:format)          appointments#index
-#              artists POST   /artists(.:format)                                       artists#create
-#           new_artist GET    /artists/new(.:format)                                   artists#new
-#          edit_artist GET    /artists/:id/edit(.:format)                              artists#edit
-#               artist GET    /artists/:id(.:format)                                   artists#show
-#                      PATCH  /artists/:id(.:format)                                   artists#update
-#                      PUT    /artists/:id(.:format)                                   artists#update
-#                      DELETE /artists/:id(.:format)                                   artists#destroy
+#               Prefix Verb   URI Pattern                                 Controller#Action
+#     artist_dashboard GET    /artists/:artist_id/dashboard        artists/dashboard#show
+#       artist_tattoos POST   /artists/:artist_id/tattoos                    tattoos#create
+#    new_artist_tattoo GET    /artists/:artist_id/tattoos/new                tattoos#new
+#   edit_artist_tattoo GET    /artists/:artist_id/tattoos/:id/edit           tattoos#edit
+#        artist_tattoo PATCH  /artists/:artist_id/tattoos/:id                tattoos#update
+#                      PUT    /artists/:artist_id/tattoos/:id                tattoos#update
+#                      DELETE /artists/:artist_id/tattoos/:id                tattoos#destroy
+#  artist_appointments GET    /artists/:artist_id/appointments          appointments#index
+#              artists POST   /artists                                       artists#create
+#           new_artist GET    /artists/new                                   artists#new
+#          edit_artist GET    /artists/:id/edit                              artists#edit
+#               artist GET    /artists/:id                                   artists#show
+#                      PATCH  /artists/:id                                   artists#update
+#                      PUT    /artists/:id                                   artists#update
+#                      DELETE /artists/:id                                   artists#destroy
