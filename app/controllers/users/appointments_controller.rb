15590f60462f238c20c5795ab6dabe83c6034922
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-15 23:55:08 -0600

    basic appts view setup

--- app/controllers/users/appointments_controller.rb
+++ app/controllers/users/appointments_controller.rb
@@ -2,7 +2,7 @@ class Users::AppointmentsController < ApplicationController
   def index
     user_id = params[:user_id]
     @user = UserFacade.user_data(user_id)
-    @tattoos = UserFacade.appointments(user_id)
+    # @appointments = UserFacade.appointments(user_id)
     render "users/appointments"
   end
 end
