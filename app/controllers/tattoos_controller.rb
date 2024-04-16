dce8951c423a42d76d8d89a909d47ac9860a0a31
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-15 19:30:29 -0600

    Feat: add update action to tattoos controller

--- app/controllers/tattoos_controller.rb
+++ app/controllers/tattoos_controller.rb
@@ -21,4 +21,29 @@ class TattoosController < ApplicationController
       flash[:error] = "Tattoo could not be uploaded"
     end
   end
+
+  def edit
+    @artist_id = params[:artist_id]
+    @tattoo = ArtistFacade.new.find_tattoo(params[:id])
+  end
+
+  def update
+    tattoo = ArtistFacade.new.find_tattoo(params[:id])
+    
+    tattoo_attributes = {price: params[:price], time_estimate: params[:time_estimate], artist_id: params[:artist_id], image_url: tattoo.image_url}
+    
+    tattoo = ArtistFacade.new.update_tattoo(tattoo_attributes)
+
+    if tattoo.valid?
+      redirect_to artist_dashboard_path(params[:artist_id])
+      flash[:success] = "Tattoo updated successfully"
+    else
+      redirect_to edit_artist_tattoo_path(params[:artist_id])
+      flash[:error] = "Tattoo could not be uploaded"
+    end
+  end
+
+  def delete
+
+  end
 end
