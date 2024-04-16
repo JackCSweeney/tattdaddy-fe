47366415344aa28b565c2d6eb59c8e9e4c181607
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-16 16:06:22 -0600

    Fix poro, removing validations

--- app/poros/tattoo.rb
+++ app/poros/tattoo.rb
@@ -1,16 +1,9 @@
 class Tattoo
-  include ActiveModel::Model
-
   attr_reader :id,
               :image_url,
               :price,
               :time_estimate,
               :artist_id
-
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :time_estimate, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :image_url, presence: true
-  validates :artist_id, presence: true
 
   def initialize(attributes)
     @id = attributes[:id]
