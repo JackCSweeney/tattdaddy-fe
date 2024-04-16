11c262811314a319c3371c385be76d702d868c37
Jack Sweeney <jackcsweeney@gmail.com>
2024-04-16 08:18:44 -0700

    feat: Add create_artist_identities and create_artist to ArtistService

--- app/services/artist_service.rb
+++ app/services/artist_service.rb
@@ -19,4 +19,19 @@ class ArtistService
   def artist_tattoos(id)
     get_url("/api/v0/artists/#{id}/tattoos")
   end
+
+  def post_url(url, params)
+    response = connection.post(url, params)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def create_artist(artist_attributes)
+    post_url("/api/v0/artists", {artist: artist_attributes})
+  end
+
+  def create_artist_identities(identities, artist_id)
+    identities.map do |identity_id|
+      post_url("/api/v0/artist_identities", {artist_identity: {artist_id: artist_id, identity_id: identity_id}})
+    end
+  end
 end
