4ba7f7e7ab192097de7d885367f1eaacc87c672e
Jack Sweeney <jackcsweeney@gmail.com>
2024-04-17 14:54:09 -0700

    feat: Make cache_key_with_version method in application helper to utilize with retrieval of dashboard tattoos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def cache_key_with_version(model_class, label = "")
+    prefix = model_class.to_s.downcase.pluralize
+    id = model_class.id
+    [prefix, label, id].join("-")
+  end
 end
