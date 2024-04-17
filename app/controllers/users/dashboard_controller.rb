87f607977ce4f298e6e614b0c89601fb8f659174
Jack Sweeney <jackcsweeney@gmail.com>
2024-04-17 15:15:43 -0700

    fix: Fix caching naming issue

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -3,8 +3,8 @@ class Users::DashboardController < ApplicationController
   def show
     user_id = params[:user_id]
     @user = UserFacade.user_data(user_id)
-    Rails.cache.fetch("#{cache_key_with_version(@user)}/dashboard_tattoos", expires_in: 10.minutes) do
-      @tattoos = UserFacade.dashboard_tattoos(user_id)
+    @tattoos = Rails.cache.fetch("#{user_id}/dashboard-tattoos", expires_in: 10.minutes) do
+      UserFacade.dashboard_tattoos(user_id)
     end
     render 'users/dashboard'
   end
