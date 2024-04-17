191ae6727d679d89687d77affef37406ddde9c71
Jack Sweeney <jackcsweeney@gmail.com>
2024-04-17 10:29:54 -0700

    fix: Make users loggin in through github not hit edit page unless they are missing attributes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController <ApplicationController
       decoded_token = JWT.decode(jwt_token, ENV['JWT_SECRET'], true, {algorithm: 'HS256'})
       if decoded_token.first['attributes']['user_id']
         user_id = decoded_token.first['attributes']['user_id']
-        @user = User.new({id: user_id, attributes: {name: decoded_token.first['attributes']["name"], email: decoded_token.first['attributes']["email"], location: decoded_token.first['attributes']["location"]}})
+        @user = User.new({id: user_id, attributes: {name: decoded_token.first['attributes']["name"], email: decoded_token.first['attributes']["email"], location: decoded_token.first['attributes']["location"], search_radius: decoded_token.first['attributes']["search_radius"]}})
         session[:user_id] = user_id
       end
 
-      if @user.attributes.any?(nil)
+      if @user.attributes.include?(nil)
         redirect_to edit_user_path(@user.id)
       elsif @user
         redirect_to user_dashboard_path(@user.id)
