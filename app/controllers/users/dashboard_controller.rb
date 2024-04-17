980993f00c16f7757173f5e39867a338e8202648
Jack Sweeney <jackcsweeney@gmail.com>
2024-04-17 14:53:23 -0700

    feat: Add caching feature to retrieval of dashboard tattoos

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -1,8 +1,11 @@
 class Users::DashboardController < ApplicationController
+  include ApplicationHelper
   def show
     user_id = params[:user_id]
     @user = UserFacade.user_data(user_id)
-    @tattoos = UserFacade.dashboard_tattoos(user_id)
+    Rails.cache.fetch("#{cache_key_with_version(@user)}/dashboard_tattoos", expires_in: 10.minutes) do
+      @tattoos = UserFacade.dashboard_tattoos(user_id)
+    end
     render 'users/dashboard'
   end
 end
