b743fab378875cc16053ce4757040754f1793c12
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-04-18 15:57:39 -0600

    final authorization

--- spec/features/artists/authorization_for_dashboard_spec.rb
+++ spec/features/artists/authorization_for_dashboard_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Artist authorization', type: :feature do
       visit artist_dashboard_path(artist_id: 5)
 
       expect(current_path).to eq(root_path)
-
+      
       expect(page).to have_content("You need to login")
 
     end
