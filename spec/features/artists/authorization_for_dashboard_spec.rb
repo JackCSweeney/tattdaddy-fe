84239fc4b138dfe6c05fd37d8db50376abd8ebcf
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-04-18 11:03:38 -0600

    authorization finalized

---
+++ spec/features/artists/authorization_for_dashboard_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Artist authorization', type: :feature do
+  describe 'As a logged_out artist' do
+    it 'cannot go to dashboard when not signed in' do
+      visit artist_dashboard_path(artist_id: 5)
+
+      expect(current_path).to eq(root_path)
+
+      expect(page).to have_content("You need to login")
+
+    end
+  end
+end
