6cf7726696437f93dc15c58a1dae63c7d7d9ab0a
Jack Sweeney <jackcsweeney@gmail.com>
2024-04-18 05:52:12 -0700

    feat: Update geolocate action in geolocation controller to utilize browser permission coords for location

--- app/controllers/geolocation_controller.rb
+++ app/controllers/geolocation_controller.rb
@@ -1,7 +1,6 @@
 class GeolocationController < ApplicationController
   def geolocate
-    coords = GeolocateFacade.find_coords
-    address = GeolocateFacade.find_address(coords)
+    address = GeolocateFacade.find_address(params[:coords])
     UserFacade.update_user_data(params[:id], {location: address})
     redirect_to user_dashboard_path(params[:id])
   end
