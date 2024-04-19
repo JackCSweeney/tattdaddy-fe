008023bb04b1ce93083b79ef127b621c0a1aff3c
Jack Sweeney <jackcsweeney@gmail.com>
2024-04-19 06:47:03 -0700

    fix user liked tattoo layout issue

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -5,7 +5,7 @@ class Users::DashboardController < ApplicationController
     user_id = params[:user_id]
     if session[:user_id].present? && session[:user_id].to_i == user_id.to_i
       @user = UserFacade.user_data(user_id)
-      @tattoos = Rails.cache.fetch("#{cache_key_with_version(@user)}-dashboard-tattoos", expires_in: 10.minutes) do
+      @tattoos = Rails.cache.fetch("#{cache_key_with_version(@user)}-dashboard-tattoos", expires_in: 1.minutes) do
         UserFacade.dashboard_tattoos(user_id)
       end
       render 'users/dashboard'
